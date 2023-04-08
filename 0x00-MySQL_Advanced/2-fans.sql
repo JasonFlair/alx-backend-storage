653bc05ea75cadc5994f0c3de94e6df6704bb2c7
Chukwuemelie Obumse <69307904+JasonFlair@users.noreply.github.com>
2023-04-08 05:33:38 +0100

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,12 +1,6 @@
 -- This SQL script select origin column, and sum of fans column as nb_fans, grouped by origin and ordered by nb_fans descending from 'metal_bands' table.
 
 SELECT origin, SUM(fans) AS nb_fans
-<<<<<<< HEAD
        FROM metal_bands
        GROUP BY origin
        ORDER BY nb_fans DESC;
-=======
-    FROM metal_bands
-    GROUP BY origin
-    ORDER BY nb_fans DESC;
->>>>>>> e54a0a20c6440709a803cfeb73baadca467a2381
