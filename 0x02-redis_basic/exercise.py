fb52b88c83f39b6f59812b4cbcaea54418c2e498
Jason Flair <emelieobumse100@gmail.com>
2023-04-13 00:30:37 +0100

    redis tasks done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -76,6 +76,7 @@ class Cache:
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """method that takes a data argument and returns a string"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
