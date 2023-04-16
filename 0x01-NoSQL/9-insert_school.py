a2ca0393d080fd7c8b5d3125f486afb84e4aafd2
Chukwuemelie Obumse <69307904+JasonFlair@users.noreply.github.com>
2023-04-16 16:16:15 +0100

    insert a document, observation commented

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -9,5 +9,8 @@ def insert_school(mongo_collection, **kwargs):
     """
     inserts a document into a collection
     """
+    # data returned is stored and id is returned
     data = mongo_collection.insert_one(kwargs)
     return data.inserted_id
+    # i also noticed returning data itself will 
+    # simply return the id.
